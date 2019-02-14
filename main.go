d1e5033727b689fd19392d7670dec8c7cae6185e
Ronald Doorn <rdoorn@schubergphilis.com>
2019-02-14 11:22:22 +0100

    retry

--- main.go
+++ main.go
@@ -71,10 +71,11 @@ func (d *DeviceList) scan() {
 }
 
 func (d *DeviceList) query() {
-	for id, dev := range d.Devices {
+	for _, dev := range d.Devices {
 		if dev.Name == "" {
 			fmt.Printf("Querying %s...\n", dev.Addr.String())
-			d.queryHandler(id)
+			//d.queryHandler(id)
+			caller(dev.Addr.String())
 		}
 	}
 }
@@ -108,7 +109,8 @@ func (d *DeviceList) queryHandler(id int) {
 	ctx := ble.WithSigHandler(context.WithTimeout(context.Background(), 2*time.Second))
 	cln, err := ble.Connect(ctx, filter)
 	if err != nil {
-		log.Fatalf("can't connect : %s", err)
+		log.Printf("can't connect : %s", err)
+		return
 	}
 
 	// Make sure we had the chance to print out the message.
@@ -187,6 +189,120 @@ func explore(cln ble.Client, p *ble.Profile, d *Device) error {
 				if c.UUID.String() == "2a24" { // device name
 					d.Type = string(b)
 				}
+			}
+
+			for _, d := range c.Descriptors {
+				fmt.Printf("        Descriptor: %s %s, Handle(0x%02x)\n", d.UUID, ble.Name(d.UUID), d.Handle)
+				b, err := cln.ReadDescriptor(d)
+				if err != nil {
+					fmt.Printf("Failed to read descriptor: %s\n", err)
+					continue
+				}
+				fmt.Printf("        Value         %x | %q\n", b, b)
+			}
+
+			/*
+				if *sub != 0 {
+					// Don't bother to subscribe the Service Changed characteristics.
+					if c.UUID.Equal(ble.ServiceChangedUUID) {
+						continue
+					}
+
+					// Don't touch the Apple-specific Service/Characteristic.
+					// Service: D0611E78BBB44591A5F8487910AE4366
+					// Characteristic: 8667556C9A374C9184ED54EE27D90049, Property: 0x18 (WN),
+					//   Descriptor: 2902, Client Characteristic Configuration
+					//   Value         0000 | "\x00\x00"
+					if c.UUID.Equal(ble.MustParse("8667556C9A374C9184ED54EE27D90049")) {
+						continue
+					}
+
+					if (c.Property & ble.CharNotify) != 0 {
+						fmt.Printf("\n-- Subscribe to notification for %s --\n", *sub)
+						h := func(req []byte) { fmt.Printf("Notified: %q [ % X ]\n", string(req), req) }
+						if err := cln.Subscribe(c, false, h); err != nil {
+							log.Fatalf("subscribe failed: %s", err)
+						}
+						time.Sleep(*sub)
+						if err := cln.Unsubscribe(c, false); err != nil {
+							log.Fatalf("unsubscribe failed: %s", err)
+						}
+						fmt.Printf("-- Unsubscribe to notification --\n")
+					}
+					if (c.Property & ble.CharIndicate) != 0 {
+						fmt.Printf("\n-- Subscribe to indication of %s --\n", *sub)
+						h := func(req []byte) { fmt.Printf("Indicated: %q [ % X ]\n", string(req), req) }
+						if err := cln.Subscribe(c, true, h); err != nil {
+							log.Fatalf("subscribe failed: %s", err)
+						}
+						time.Sleep(*sub)
+						if err := cln.Unsubscribe(c, true); err != nil {
+							log.Fatalf("unsubscribe failed: %s", err)
+						}
+						fmt.Printf("-- Unsubscribe to indication --\n")
+					}
+				}
+			*/
+		}
+		fmt.Printf("\n")
+	}
+	return nil
+}
+
+func caller(addr string) {
+	filter := func(a ble.Advertisement) bool {
+		return strings.ToUpper(a.Addr().String()) == strings.ToUpper(addr)
+	}
+
+	// Scan for specified durantion, or until interrupted by user.
+	fmt.Printf("Querying for %s...\n")
+	ctx := ble.WithSigHandler(context.WithTimeout(context.Background(), 2*time.Second))
+	cln, err := ble.Connect(ctx, filter)
+	if err != nil {
+		log.Fatalf("can't connect : %s", err)
+	}
+
+	// Make sure we had the chance to print out the message.
+	done := make(chan struct{})
+	// Normally, the connection is disconnected by us after our exploration.
+	// However, it can be asynchronously disconnected by the remote peripheral.
+	// So we wait(detect) the disconnection in the go routine.
+	go func() {
+		<-cln.Disconnected()
+		fmt.Printf("[ %s ] is disconnected \n", cln.Addr())
+		close(done)
+	}()
+
+	fmt.Printf("Discovering profile...\n")
+	p, err := cln.DiscoverProfile(true)
+	if err != nil {
+		log.Fatalf("can't discover profile: %s", err)
+	}
+
+	// Start the exploration.
+	explore2(cln, p)
+
+	// Disconnect the connection. (On OS X, this might take a while.)
+	fmt.Printf("Disconnecting [ %s ]... (this might take up to few seconds on OS X)\n", cln.Addr())
+	cln.CancelConnection()
+
+	<-done
+}
+
+func explore2(cln ble.Client, p *ble.Profile) error {
+	for _, s := range p.Services {
+		fmt.Printf("    Service: %s %s, Handle (0x%02X)\n", s.UUID, ble.Name(s.UUID), s.Handle)
+
+		for _, c := range s.Characteristics {
+			fmt.Printf("      Characteristic: %s %s, Property: 0x%02X (%s), Handle(0x%02X), VHandle(0x%02X)\n",
+				c.UUID, ble.Name(c.UUID), c.Property, propString(c.Property), c.Handle, c.ValueHandle)
+			if (c.Property & ble.CharRead) != 0 {
+				b, err := cln.ReadCharacteristic(c)
+				if err != nil {
+					fmt.Printf("Failed to read characteristic: %s\n", err)
+					continue
+				}
+				fmt.Printf("        Value         %x | %q\n", b, b)
 			}
 
 			for _, d := range c.Descriptors {
